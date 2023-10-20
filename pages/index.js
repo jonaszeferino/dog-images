8eb6aabd96d750d58c279d013cadeb874bc39f6e
jonaszeferino <jonaszeferino@gmail.com>
2023-10-19 21:11:18 -0300

    dog images with chakra

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,14 @@
 import { useEffect, useState } from "react";
 import styles from "./index.module.css";
 import Image from "next/image";
-// import { useRouter } from "next/router";
+import { Button, ChakraProvider, Center, BeatLoader } from "@chakra-ui/react";
 
 let url = "https://dog.ceo/api/breeds/image/random";
 
 export default function Home() {
   let [dogfact, setdogfact] = useState();
   let [dogCount, setDogCount] = useState(-1);
-  //  useEffect(() => {
-  //     apiCall();
-  //    console.log('foi chamado - ou seja quando ele carrega a página')
-  //  }, []);
+
   useEffect(() => {
     console.log("mais uma imagem chamada");
     setDogCount((dogCount += 1));
@@ -24,41 +21,46 @@ export default function Home() {
   };
 
   return (
-    <div style={{ color: "blue", display: "flex", justifyContent: "center" }}>
-      <div className={styles.title}></div>
-
-      <button
-        onClick={apiCall}
-        style={{ color: "blue", display: "flex", justifyContent: "flex-start" }}
-      >
-        Botão!
-      </button>
-
-      <Image
-        src={dogfact}
-        alt="dog"
-        layout="fixed"
-        width="600px"
-        height="500px"
-      />
-      <br />
+    <>
       <div
         style={{
-          fontSize: "50px",
-          marginTop: "10px",
-          alignContent: "flex-start",
+          color: "blue",
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "100px",
         }}
       >
+        <div className={styles.title}></div>
+
+        <Image
+          src={dogfact}
+          alt="dog"
+          layout="fixed"
+          width="600"
+          height="500"
+        />
         <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        {dogCount}
+        <div
+          style={{
+            fontSize: "50px",
+            marginTop: "10px",
+            alignContent: "flex-start",
+          }}
+        >
+          <br />
+        </div>
       </div>
-    </div>
+      <ChakraProvider>
+        <Center>{dogCount}</Center>
+      </ChakraProvider>
+      <br />
+      <ChakraProvider>
+        <Center>
+          <Button colorScheme="whatsapp" onClick={apiCall}>
+            Novo
+          </Button>
+        </Center>
+      </ChakraProvider>
+    </>
   );
 }
