1636b93c6211e1fe5a9b29f259c1c0db62b2e654
jonaszeferino <jonaszeferino@gmail.com>
2023-10-19 21:58:00 -0300

    dog images with chakra and treatments

--- pages/index.js
+++ pages/index.js
@@ -27,24 +27,40 @@ export default function Home() {
 
   return (
     <>
+      <ChakraProvider>
+        <Center>
+          <h1
+            style={{
+              color: "black",
+              fontSize: "2rem", // Tamanho da fonte em "rem"
+              marginTop: "100px",
+            }}
+          >
+            <strong>Random Dogs</strong>
+          </h1>
+        </Center>
+      </ChakraProvider>
+
       <div
         style={{
           color: "blue",
           display: "flex",
           justifyContent: "center",
-          marginTop: "100px",
+          marginTop: "50px",
         }}
       >
         <div className={styles.title}></div>
-        <ChakraProvider>
-          <ImageChakra
-            borderRadius="25px"
-            objectFit="cover"
-            boxSize="400px"
-            src={dogfact}
-            alt="Dan Abramov"
-          />
-        </ChakraProvider>
+        {dogfact ? (
+          <ChakraProvider>
+            <ImageChakra
+              borderRadius="25px"
+              objectFit="cover"
+              boxSize="500px"
+              src={dogfact}
+              alt="Random Dog"
+            />
+          </ChakraProvider>
+        ): "Click On Button New"}
 
         {/* objectFit cut image with the size of the boxSize */}
         <br />
@@ -67,7 +83,7 @@ export default function Home() {
       <ChakraProvider>
         <Center>
           <Button colorScheme="whatsapp" onClick={apiCall}>
-            Novo
+            New
           </Button>
         </Center>
       </ChakraProvider>
